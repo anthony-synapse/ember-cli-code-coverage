ef696c10924c953673de798e3db61ef3a4050086
Anthony Miller <anthony.miller@synapsemx.com>
2026-07-02 14:34:35 -0700

    update forceModulesToBeLoaded to work with vite

--- packages/ember-cli-code-coverage/addon-test-support/index.js
+++ packages/ember-cli-code-coverage/addon-test-support/index.js
@@ -1,9 +1,3 @@
-let __require;
-
-if (typeof __webpack_require__ !== 'undefined') {
-  __require = __webpack_require__; // eslint-disable-line no-undef
-}
-
 // This function exists to force the evaluation of modules that might not get loaded during
 // the normal flow of your application and thus would not show up in the coverage report. This is needed
 // because the istanbul plugin simply traverses a file and inserts counters over all lines of code
@@ -19,49 +13,30 @@ if (typeof __webpack_require__ !== 'undefined') {
 // file will makes sure all modules (including those from webpack where Emboider will place some) get
 // evaluated by actually requiring them.
 //
-export function forceModulesToBeLoaded(filterFunction) {
+//
+export async function forceModulesToBeLoaded(filterFunction) {
   if (!filterFunction) {
-    filterFunction = (type, module) => {
-      if (type === 'webpack') {
-        return (
-          !module.startsWith('../') && !module.startsWith('./node_modules/')
-        );
-      }
-
-      let excludeTestModules = /^[^/]+\/tests\//;
-      return !excludeTestModules.test(module);
+    filterFunction = (_type, modulePath) => {
+      const excludeTestModules = /^[^/]+\/tests\//;
+      return !excludeTestModules.test(modulePath);
     };
   }
 
-  // this should always be ran **after** the test suite is completed since
-  // sometimes force calling modules can have side effects and invoking them
-  // after the app has run can reduce problems that would arrise.
-  if (typeof __webpack_require__ !== 'undefined') {
-    let __modules = __require.m;
-    Object.keys(__modules).forEach((moduleName) => {
+  const entries = Object.entries(modules).filter(([path]) =>
+    filterFunction('vite', path),
+  );
+
+  await Promise.all(
+    entries.map(async ([path, importModule]) => {
       try {
-        if (filterFunction('webpack', moduleName)) {
-          __require(moduleName);
-        }
+        await importModule();
       } catch (error) {
         console.warn(
-          `Error occurred while evaluating '${moduleName}': ${error.message}\n${error.stack}`
+          `Error occurred while evaluating '${path}': ${error.message}\n${error.stack}`,
         );
       }
-    });
-  }
-
-  Object.keys(window.requirejs.entries).forEach((moduleName) => {
-    try {
-      if (filterFunction('require', moduleName)) {
-        require(moduleName); // eslint-disable-line no-undef
-      }
-    } catch (error) {
-      console.warn(
-        `Error occurred while evaluating '${moduleName}': ${error.message}\n${error.stack}`
-      );
-    }
-  });
+    }),
+  );
 }
 
 export async function sendCoverage(callback) {
